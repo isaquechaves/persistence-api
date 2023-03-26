f0d7971ad8fa0e31da594d6ac18e223d9a8f2e7d
Isaque Chaves <isaque.oliveira2@fatec.sp.gov.br>
2023-03-26 13:14:08 -0300

    Initial commit

---
+++ src/test/java/com/fatec/stecktec/persistenceapi/PersistenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.stecktec.persistenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
